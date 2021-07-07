7725fc16edd8a9c5f8ffd2e30c8879256b9804ec
maxcserrano <maxiroserrano@gmail.com>
2021-07-07 18:02:40 -0500

    detalles

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, function() {
     console.log('Servidor funcionando en el puerto 3000');
 });
 
